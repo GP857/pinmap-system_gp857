fffa3093a03e33b17c5ee2d8793d40419e010dd4
GUSTAVO PINHEIRO <gp70ia@gmail.com>
2025-08-01 18:47:00 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,7 +1,7 @@
 // dados.js
-// Exporta a variável 'dados' para que outros módulos possam importá-la.
-export const dados = [
-    {
+const locations = [
+  // ... seus pontos aqui ...
+];
     
     "id": 1,
     "nome": "Juliane Ap. Cyrilo Cunha",
