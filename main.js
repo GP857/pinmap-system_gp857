9594c7bef4b176797676fdaf8b82d11853018838
GUSTAVO PINHEIRO <gp70ia@gmail.com>
2025-08-01 13:32:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
+import { usuarios } from './usuarios.js';
+
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -22.9, lng: -47.05 },
-    zoom: 10,
-    mapId: "DEMO_MAP_ID"
+  const mapa = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -22.9099, lng: -47.0626 },
+    zoom: 8,
   });
 
-  const infoWindow = new google.maps.InfoWindow();
-  const markers = usuarios.map(usuario => {
+  usuarios.forEach(usuario => {
     const marker = new google.maps.Marker({
       position: { lat: usuario.latitude, lng: usuario.longitude },
-      icon: usuario.icone,
+      map: mapa,
       title: usuario.nome,
     });
-
-    marker.addListener("click", () => {
-      infoWindow.setContent(`
-        <strong>${usuario.nome}</strong><br>
-        ${usuario.localizacao}<br>
-        <a href="${usuario.link}" target="_blank">Ver no mapa</a>
-      `);
-      infoWindow.open(map, marker);
-    });
-
-    return marker;
   });
-
-  new markerClusterer.MarkerClusterer({ map, markers });
 }
+
+window.initMap = initMap;
