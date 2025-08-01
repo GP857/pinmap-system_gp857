cdc019de75b0c1204f5af9512afa335acfccd8b5
GUSTAVO PINHEIRO <gp70ia@gmail.com>
2025-08-01 13:31:23 -0300

    Update sistemaBuscaGoogleMaps.js

--- sistemaBuscaGoogleMaps.js
+++ sistemaBuscaGoogleMaps.js
@@ -1,59 +1,44 @@
-// sistemaBuscaGoogleMaps.js
-class SistemaBuscaMapaGoogle {
+export function inicializarSistemaBuscaGoogleMaps(mapa) {
+  class SistemaBuscaMapaGoogle {
     constructor(mapa) {
-        this.mapa = mapa;
-        this.markers = [];
-        this.AdvancedMarkerElement = null;
-        this.InfoWindow = null;
-        this.Geocoder = null;
-    }
-
-    async init() {
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-        const { InfoWindow } = await google.maps.importLibrary("maps");
-        const { Geocoder } = await google.maps.importLibrary("geocoding");
-        this.AdvancedMarkerElement = AdvancedMarkerElement;
-        this.InfoWindow = new InfoWindow();
-        this.Geocoder = new Geocoder();
-        console.log('✔️ Módulo de busca inicializado.');
+      this.mapa = mapa;
+      this.markers = [];
+      this.infoWindow = new google.maps.InfoWindow();
     }
 
     limparMarcadores() {
-        this.markers.forEach(marker => marker.map = null);
-        this.markers = [];
+      for (const marker of this.markers) {
+        marker.setMap(null);
+      }
+      this.markers = [];
     }
 
-    adicionarMarcador(posicao, titulo, descricao, link) {
-        const marker = new this.AdvancedMarkerElement({ position: posicao, map: this.mapa, title: titulo });
-        marker.addListener('click', () => {
-            const content = `<div><h4>${titulo}</h4><p>${descricao}</p>${link ? `<a href="${link}" target="_blank" class="btn-detalhes">Ver detalhes</a>` : ''}</div>`;
-            this.InfoWindow.setContent(content);
-            this.InfoWindow.open(this.mapa, marker);
-        });
-        this.markers.push(marker);
-    }
-
-    async buscarPorEndereco(endereco) {
-        try {
-            const response = await this.Geocoder.geocode({ address: endereco });
-            if (response.results[0]) {
-                return response.results[0].geometry.location;
-            }
-            throw new Error('Nenhum resultado encontrado.');
-        } catch (error) {
-            console.error(`Geocoding falhou para "${endereco}":`, error);
-            throw new Error('Endereço não encontrado.');
+    adicionarMarcador(posicao, titulo, descricao, linkDetalhes) {
+      const marker = new google.maps.Marker({
+        position: posicao,
+        map: this.mapa,
+        title: titulo,
+        icon: {
+          url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png', // ou seu ícone SVG/PNG
+          scaledSize: new google.maps.Size(32, 32)
         }
-    }
+      });
 
-    centralizarMapaNaPosicao(posicao) {
-        this.mapa.setCenter(posicao);
-        this.mapa.setZoom(15);
-    }
-}
+      marker.addListener('click', () => {
+        const conteudo = `
+          <div style="max-width: 200px;">
+            <h3>${titulo}</h3>
+            <p>${descricao}</p>
+            <a href="${linkDetalhes}" target="_blank">Ver mais</a>
+          </div>
+        `;
+        this.infoWindow.setContent(conteudo);
+        this.infoWindow.open(this.mapa, marker);
+      });
 
-export async function inicializarSistemaBuscaGoogleMaps(mapa) {
-    const sistemaBusca = new SistemaBuscaMapaGoogle(mapa);
-    await sistemaBusca.init();
-    return sistemaBusca;
+      this.markers.push(marker);
+    }
+  }
+
+  return new SistemaBuscaMapaGoogle(mapa);
 }
